ec455e16e2ccf4c31b3488903d0967903fb8b7ba
edelwud <edelwud@icloud.com>
2021-03-03 03:50:44 +0300

    implement: cached matrix factory

--- vectorization/src/cached_vectorized/cached_vectorized.cpp
+++ vectorization/src/cached_vectorized/cached_vectorized.cpp
@@ -1,10 +1,31 @@
-#include <matrix/cached_matrix.hpp>
-#include <matrix/parent_matrix.hpp>
 #include <benchmark/benchmark.hpp>
+#include <matrix/parent_matrix.hpp>
+#include <matrix/factory/cached_matrix.hpp>
 
 int main() {
-    CachedMatrix matrix(MATRIX_BLOCK_SIDE*5, MATRIX_BLOCK_SIDE*5);
-    CachedMatrix matrix2(MATRIX_BLOCK_SIDE*5, MATRIX_BLOCK_SIDE*5);
-    std::cout << Benchmark::getExecutionTime([&matrix, &matrix2](){matrix.multiply(&matrix2);});
+    CachedMatrixFactory factory;
+    ParentMatrix matrix(
+    {
+            3,
+            3,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    ParentMatrix matrix2(
+        {
+            3,
+            3,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    std::cout << Benchmark::getExecutionTime(
+        [&matrix, &matrix2](){
+            matrix.multiply(matrix2);
+        }
+    );
     return 0;
 }
--- vectorization/src/manual_vectorized/manual_vectorized.cpp
+++ vectorization/src/manual_vectorized/manual_vectorized.cpp
@@ -1,12 +1,32 @@
-#include <matrix/factory/vectorized_matrix.hpp>
-#include <matrix/parent_matrix.hpp>
 #include <benchmark/benchmark.hpp>
+#include <matrix/parent_matrix.hpp>
+#include <matrix/cached_matrix.hpp>
+#include <matrix/factory/vectorized_matrix.hpp>
 
 int main() {
     VectorizedMatrixFactory factory;
-    ParentMatrix matrix({256,256,8,8}, factory);
-    ParentMatrix matrix2({256,256,8,8}, factory);
-    std::cout << Benchmark::getExecutionTime([&matrix, &matrix2](){matrix.multiply(matrix2);});
-
+    ParentMatrix matrix(
+        {
+            3,
+            3,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    ParentMatrix matrix2(
+        {
+            3,
+            3,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    std::cout << Benchmark::getExecutionTime(
+        [&matrix, &matrix2](){
+            matrix.multiply(matrix2);
+        }
+    );
     return 0;
 }
