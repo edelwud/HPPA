1b1acfc9ebe544f4884fcf901f10255ecdba6644
edelwud <edelwud@icloud.com>
2021-03-03 04:22:43 +0300

    implement: cached matrix tests

--- vectorization/libs/matrix/tests/test_cached_matrix.cpp
+++ vectorization/libs/matrix/tests/test_cached_matrix.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
+
 #include <matrix/cached_matrix.hpp>
 #include <matrix/vectorized_matrix.hpp>
-
-#include <cmath>
+#include <matrix/parent_matrix.hpp>
+#include <matrix/factory/vectorized_matrix.hpp>
+#include <matrix/factory/cached_matrix.hpp>
 
 TEST(cached_matrix, AllocateTest) {
     CachedMatrix matrix(1, 2);
@@ -26,14 +28,60 @@ TEST(cached_matrix, AddMatrixTest) {
     matrix1.print();
 }
 
-TEST(cached_matrix, CompareWithStandardTest) {
-    CachedMatrix matrix1(MATRIX_BLOCK_SIDE*10, MATRIX_BLOCK_SIDE*10);
-    CachedMatrix matrix2(MATRIX_BLOCK_SIDE*10, MATRIX_BLOCK_SIDE*10);
+TEST(cached_matrix, CompareWithVectorizedTest) {
+    CachedMatrix matrix1(MATRIX_BLOCK_SIDE, MATRIX_BLOCK_SIDE);
+    CachedMatrix matrix2(MATRIX_BLOCK_SIDE, MATRIX_BLOCK_SIDE);
     matrix1.multiply(&matrix2);
 
-    VectorizedMatrix matrix3(MATRIX_BLOCK_SIDE*10, MATRIX_BLOCK_SIDE*10);
-    VectorizedMatrix matrix4(MATRIX_BLOCK_SIDE*10, MATRIX_BLOCK_SIDE*10);
+    VectorizedMatrix matrix3(MATRIX_BLOCK_SIDE, MATRIX_BLOCK_SIDE);
+    VectorizedMatrix matrix4(MATRIX_BLOCK_SIDE, MATRIX_BLOCK_SIDE);
     matrix3.multiply(&matrix4);
 
     ASSERT_EQ(matrix1, matrix3);
+}
+
+TEST(cached_matrix, CompareWithParentTest) {
+    CachedMatrixFactory factory;
+    ParentMatrix matrix(
+        {
+            2,
+            2,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    ParentMatrix matrix2(
+        {
+            2,
+            2,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factory
+    );
+    matrix.multiply(matrix2);
+
+    VectorizedMatrixFactory factoryVec;
+    ParentMatrix matrix3(
+        {
+            2,
+            2,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factoryVec
+    );
+    ParentMatrix matrix4(
+        {
+            2,
+            2,
+            MATRIX_BLOCK_SIDE*2,
+            MATRIX_BLOCK_SIDE*2
+        },
+        factoryVec
+    );
+    matrix3.multiply(matrix4);
+
+    ASSERT_EQ(matrix, matrix3);
 }
