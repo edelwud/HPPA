64eb7afa2f7ec74f2c9bdf7362397039ebf2aa65
edelwud <edelwud@icloud.com>
2021-05-06 22:18:20 +0300

    add: pgm file

--- image-processing/libs/loader/tests/loader_test.cpp
+++ image-processing/libs/loader/tests/loader_test.cpp
@@ -2,9 +2,14 @@
 
 #include <loader/loader.hpp>
 
-TEST(loader, LoadingImageTest) {
+TEST(loader, LoadingColorImageTest) {
     auto image = Loader::loadImage(LOADER_ASSETS_PATH + "sample.ppm", 3);
     ASSERT_EQ(image.width, 640);
+}
+
+TEST(loader, LoadingGrayscaleImageTest) {
+    auto image = Loader::loadImage(LOADER_ASSETS_PATH + "sample.pgm", 1);
+    ASSERT_EQ(image.width, 5184);
 }
 
 int main(int argc, char *argv[]) {
