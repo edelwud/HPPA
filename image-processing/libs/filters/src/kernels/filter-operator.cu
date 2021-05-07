bb080ba8ca6fb5b42c86292ee914753c68fdb1b2
edelwud <edelwud@icloud.com>
2021-05-07 14:56:23 +0300

    revret: old memory mapping

--- image-processing/libs/filters/src/kernels/filter-operator.cu
+++ image-processing/libs/filters/src/kernels/filter-operator.cu
@@ -6,21 +6,6 @@ __global__ void filterOperator(const char *filter, unsigned char *devSource, uns
     auto offsetX = (blockDim.x * blockIdx.x + threadIdx.x) * 4 + 1;
     auto offsetY = (blockDim.y * blockIdx.y + threadIdx.y) + 1;
 
-    auto sharedX = threadIdx.x + 1;
-    auto sharedY = threadIdx.y + 1;
-
-    __shared__ char sharedMemory[18][130];
-
-#pragma unroll
-    for (int i = -1; i < 2; i++) {
-        memcpy(
-                sharedMemory[sharedY + i] + sharedX - 1,
-                devSource + (offsetY + i) * pitch + offsetX - 1,
-                6
-        );
-    }
-
-    __syncthreads();
 
 #pragma unroll
     for (int i = 0; i < 4; i++) {
@@ -29,7 +14,7 @@ __global__ void filterOperator(const char *filter, unsigned char *devSource, uns
         for (int l = -1, k = 0; k < 3; l++, k++) {
 #pragma unroll
             for (int j = -1, m = 0; m < 3; j++, m++) {
-                result += sharedMemory[sharedY + l][sharedX + j] * filter[k * 3 + m];
+                result += devSource[(offsetY + l) * pitch + offsetX + j + i] * filter[k * 3 + m];
             }
         }
 
